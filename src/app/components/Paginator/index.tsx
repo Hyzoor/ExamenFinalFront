ec83d1099a17898c8b58c1aa13fccdf920ed9cb0
Fernando <mkiii@hotmail.es>
2026-05-25 14:32:45 +0200

    no ocnsigo el pages shortcuts

--- src/app/components/Paginator/index.tsx
+++ src/app/components/Paginator/index.tsx
@@ -9,8 +9,7 @@ const Paginator = ({ pages, page, next, prev, setPage }: {
 	setPage: (page: number) => void
 }) => {
 
-
-	const indexes:number[] = [0, 1, 2, page, pages - 2, pages - 1, pages];
+	const indexes: number[] = [0, 1, 2, page, pages - 2, pages - 1, pages];
 
 	const pagesShortcuts = indexes.filter((x, index) => {
 		indexes.indexOf(x) === index
@@ -18,10 +17,9 @@ const Paginator = ({ pages, page, next, prev, setPage }: {
 
 	const sortedPages = pagesShortcuts.sort((n1, n2) => n1 - n2);
 
-	
+
 	return (
 		<div className="pagination-container">
-
 
 			{prev &&
 				<div className="arrow-container" onClick={() => setPage(page - 1)}>
@@ -36,7 +34,14 @@ const Paginator = ({ pages, page, next, prev, setPage }: {
 				</div>
 			}
 
+			<>
 
+				{sortedPages.map((i) => {
+					<div className="shortcut" onClick={() => setPage(i)}>
+						{i}
+					</div>
+				})}
+			</>
 
 
 		</div>
